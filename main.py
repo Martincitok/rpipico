ce54a2b91db096734f15fd9f5fd27dbd7ab51e6e
Martin Perret <juanmartinperretzuza@gmail.com>
2025-03-20 15:15:34 -0300

    SinTry

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ led_board = Pin("LED", Pin.OUT)
 sleep(1)    #le damos tiempo a vREPL
 print("\nLED esta destellando...")
 while True:
-    try:
-        led_board.toggle()
-        # led_board.value(not led_board.value())
-        sleep(.5) # sleep 1sec
-    except KeyboardInterrupt:
-        break
+        
+    led_board.toggle()
+    # led_board.value(not led_board.value())
+    sleep(.5) # sleep 1sec
+    
 led_board.off()
 print("Listo")
